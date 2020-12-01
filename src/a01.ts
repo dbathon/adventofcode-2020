86a70fe92aed3fc910c12f477600c60d145750dc
Dominik Bathon <dominik.bathon@gmail.com>
2020-12-01 10:24:24 +0100

    Solve day 1

--- src/a01.ts
+++ src/a01.ts
@@ -1,5 +1,27 @@
 import { p, readLines } from "./util/util";
 
-const lines = readLines("input/a01.txt");
+const numbers = readLines("input/a01.txt").map(str => parseInt(str));
 
-p(lines);
+for (let i = 0; i < numbers.length; ++i) {
+  const n1 = numbers[i];
+  for (let j = i + 1; j < numbers.length; ++j) {
+    const n2 = numbers[j];
+    if (n1 + n2 === 2020) {
+      p([n1, n2, n1 * n2]);
+    }
+  }
+}
+
+
+for (let i = 0; i < numbers.length; ++i) {
+  const n1 = numbers[i];
+  for (let j = i + 1; j < numbers.length; ++j) {
+    const n2 = numbers[j];
+    for (let k = j + 1; k < numbers.length; ++k) {
+      const n3 = numbers[k];
+      if (n1 + n2 + n3 === 2020) {
+        p([n1, n2, n3, n1 * n2 * n3]);
+      }
+    }
+  }
+}
