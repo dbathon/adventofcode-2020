28edeead52eda1f1bb28b30f4760974cfd2287ac
Dominik Bathon <dominik.bathon@gmail.com>
2020-12-02 11:11:01 +0100

    Solve day 2

--- src/a02.ts
+++ src/a02.ts
@@ -1,5 +1,29 @@
-import { p, readLines } from "./util/util";
+import { p, readLines, sum } from "./util/util";
 
 const lines = readLines("input/a02.txt");
 
 p(lines);
+
+let validCount1 = 0;
+let validCount2 = 0;
+
+lines.forEach(line => {
+  let [spec, password] = line.split(":");
+  password = password.trim();
+  const [minMax, specChar] = spec.split(" ");
+  const [min, max] = minMax.split("-").map(str => parseInt(str));
+  const count = sum(password.split("").map(char => char === specChar ? 1 : 0));
+
+  p([min, max, specChar, password, count]);
+
+  if (min <= count && count <= max) {
+    ++validCount1;
+  }
+
+  if (sum([min, max].map(pos => password[pos - 1] === specChar ? 1 : 0)) === 1) {
+    ++validCount2;
+  }
+});
+
+p(validCount1);
+p(validCount2);
