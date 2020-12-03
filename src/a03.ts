412a631c65459dd9747abb262689c3f0c36fdece
Dominik Bathon <dominik.bathon@gmail.com>
2020-12-03 20:47:58 +0100

    Solve day 3

--- src/a03.ts
+++ src/a03.ts
@@ -2,4 +2,21 @@ import { p, readLines } from "./util/util";
 
 const lines = readLines("input/a03.txt");
 
-p(lines);
+function checkSlope(right: number, down: number): number {
+  let count = 0;
+
+  let x = 0;
+  for (let y = 0; y < lines.length; y += down) {
+    const line = lines[y];
+    const tree = line[x % line.length] === "#";
+    if (tree) {
+      ++count;
+    }
+    x += right;
+  }
+  return count;
+}
+
+p(checkSlope(3, 1));
+
+p(checkSlope(1, 1) * checkSlope(3, 1) * checkSlope(5, 1) * checkSlope(7, 1) * checkSlope(1, 2));
