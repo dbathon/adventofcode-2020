1212d645d3210f087ac2d3cae480a87b453cd091
Dominik Bathon <dominik.bathon@gmail.com>
2020-12-05 11:26:59 +0100

    Solve day 5

--- src/a05.ts
+++ src/a05.ts
@@ -2,4 +2,19 @@ import { p, readLines } from "./util/util";
 
 const lines = readLines("input/a05.txt");
 
-p(lines);
+const seatIds = lines.map(line => {
+  const binaryString = line.replaceAll(/[BR]/g, "1").replace(/[FL]/g, "0");
+  return parseInt(binaryString, 2);
+});
+
+p(Math.max(...seatIds));
+
+seatIds.sort((a, b) => a - b);
+
+let prev = seatIds[0] - 1;
+seatIds.forEach(seatId => {
+  if (prev + 2 === seatId) {
+    p(seatId - 1);
+  }
+  prev = seatId;
+});
