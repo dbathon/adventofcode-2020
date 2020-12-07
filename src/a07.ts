db9b5ec91be77b44299a8f0d5ece1826b22a50d6
Dominik Bathon <dominik.bathon@gmail.com>
2020-12-07 09:59:10 +0100

    Solve day 7

--- src/a07.ts
+++ src/a07.ts
@@ -1,5 +1,46 @@
-import { p, readLines } from "./util/util";
+import { p, readLines, sum } from "./util/util";
 
 const lines = readLines("input/a07.txt");
 
-p(lines);
+class QuantityAndColor {
+  constructor(readonly quantity: number, readonly color: string) { }
+}
+
+const rules: Map<string, QuantityAndColor[]> = new Map();
+
+lines.forEach(line => {
+  const [outerColor, containSpecs]: string[] = line.split(" bags contain ");
+  if (containSpecs !== "no other bags.") {
+    rules.set(outerColor, containSpecs.split(", ").map(containSpec => {
+      const parts = containSpec.split(" ");
+      return new QuantityAndColor(parseInt(parts[0]), parts[1] + " " + parts[2]);
+    }));
+  }
+  else {
+    rules.set(outerColor, []);
+  }
+});
+
+function canContain(outerColor: string, innerColor: string): boolean {
+  for (let quantityAndColor of (rules.get(outerColor) || [])) {
+    if (quantityAndColor.color === innerColor || canContain(quantityAndColor.color, innerColor)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+let count1 = 0;
+for (let [color] of rules) {
+  if (canContain(color, "shiny gold")) {
+    ++count1;
+  }
+}
+
+p(count1);
+
+function countTotalBags(color: string): number {
+  return 1 + sum((rules.get(color) || []).map(quantityAndColor => quantityAndColor.quantity * countTotalBags(quantityAndColor.color)));
+}
+
+p(countTotalBags("shiny gold") - 1);
