47fb6fc4da97b774f410d9d8ffbeaf9d75387ec9
Dominik Bathon <dominik.bathon@gmail.com>
2020-12-15 09:57:01 +0100

    Solve day 15

--- src/a15.ts
+++ src/a15.ts
@@ -2,4 +2,24 @@ import { p, readLines } from "./util/util";
 
 const lines = readLines("input/a15.txt");
 
-p(lines);
+const numbers = lines[0].split(",").map(num => parseInt(num));
+
+let next = numbers.pop()!;
+let nextIndex = numbers.length;
+const seenIndexes: Map<number, number> = new Map();
+numbers.forEach((number, index) => seenIndexes.set(number, index));
+
+while (nextIndex < 30000000) {
+  const lastIndex = seenIndexes.get(next);
+  seenIndexes.set(next, nextIndex);
+  if (nextIndex + 1 === 2020 || nextIndex + 1 === 30000000) {
+    p(next);
+  }
+  if (lastIndex === undefined) {
+    next = 0;
+  }
+  else {
+    next = nextIndex - lastIndex;
+  }
+  ++nextIndex;
+}
